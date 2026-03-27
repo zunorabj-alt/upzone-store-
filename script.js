44abd241cd16a284241a4a18e4ce41f86d036025
zunorabj-alt <zunorabj@gmail.com>
2026-03-27 16:14:04 +0300

    Add script.js for tab management and order sending

---
+++ script.js
@@ -0,0 +1,44 @@
+let selectedPack = "";
+
+function showTab(tab){
+  document.getElementById("ff").classList.add("hidden");
+  document.getElementById("pubg").classList.add("hidden");
+  document.getElementById(tab).classList.remove("hidden");
+
+  document.getElementById("btn-ff").classList.remove("active");
+  document.getElementById("btn-pubg").classList.remove("active");
+  document.getElementById("btn-" + tab).classList.add("active");
+}
+
+function openOrder(pack){
+  selectedPack = pack;
+  document.getElementById("formBox").classList.remove("hidden");
+  document.getElementById("selectedPack").innerText = pack;
+}
+
+function sendOrder(){
+
+  let uid = document.getElementById("uid").value;
+  let method = document.getElementById("method").value;
+  let ref = document.getElementById("ref").value;
+
+  if(uid === "" || ref === ""){
+    document.getElementById("msg").innerText = "❌ Fenoy tsara";
+    return;
+  }
+
+  emailjs.send("service_rqbn4tj", "template_2bfwhjn", {
+    uid: uid,
+    pack: selectedPack,
+    method: method,
+    ref: ref
+  }, "vFj-gepCl6oT0gavc")
+  .then(function(response){
+    document.getElementById("msg").innerText = "✅ Commande envoyée!";
+    console.log("SUCCESS:", response);
+  })
+  .catch(function(error){
+    document.getElementById("msg").innerText = "❌ Erreur envoi";
+    console.log("ERROR DETAILS:", error);
+  });
+    }
