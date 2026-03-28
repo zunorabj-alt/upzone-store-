cd769838a5cca7b90c347a9b82127a79ec28d376
zunorabj-alt <zunorabj@gmail.com>
2026-03-28 09:21:16 +0300

    Enhance payment info and user messages
    Updated payment information display with emojis and improved user prompts.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let selectedPack = "";
 function showTab(tab){
 document.getElementById("ff").classList.add("hidden");
 document.getElementById("pubg").classList.add("hidden");
-
 document.getElementById(tab).classList.remove("hidden");
 
 document.getElementById("btn-ff").classList.remove("active");
@@ -17,13 +16,15 @@ selectedPack = pack;
 document.getElementById("overlay").classList.remove("hidden");
 document.getElementById("formBox").classList.remove("hidden");
 
-document.getElementById("selectedPack").innerText = "Pack: " + pack;
+document.getElementById("selectedPack").innerText = "🎮 Pack: " + pack;
 
+/* 💳 PAYMENT INFO + EMOJIS */
 document.getElementById("msg").innerText =
-"📲 PAIEMENT:\n\n" +
-"0329768376 - JEAN FREDERIC\n" +
-"0378138688 - Marthe\n" +
-"0342981576 - Marthe";
+"📲 💳 PAIEMENT INFOS:\n\n" +
+"1️⃣ 0329768376\n   👤 JEAN FREDERIC RABENJANIRINA\n\n" +
+"2️⃣ 0378138688\n   👤 Marthe\n\n" +
+"3️⃣ 0342981576\n   👤 Marthe\n\n" +
+"⚠️ Alefaso amin’ny iray amin’ireo numéro ireo ny vola";
 }
 
 function closeOrder(){
@@ -38,22 +39,22 @@ let method = document.getElementById("method").value;
 let ref = document.getElementById("ref").value;
 
 if(uid === "" || ref === ""){
-document.getElementById("msg").innerText = "❌ Fenoy ny rehetra";
+document.getElementById("msg").innerText = "❌ Fenoy daholo azafady";
 return;
 }
 
 let number = "261344723083";
 
 let message =
-"🛒 UP ZONE STORE 💳\n\n" +
-"Pack: " + selectedPack + "\n" +
-"UID: " + uid + "\n" +
-"Méthode: " + method + "\n" +
-"Réf: " + ref + "\n\n" +
-"Paiement:\n" +
-"0329768376\n" +
-"0378138688\n" +
-"0342981576";
+"🛒 💯 UP ZONE STORE 💳\n\n" +
+"🎮 Pack: " + selectedPack + "\n" +
+"🆔 UID: " + uid + "\n" +
+"💳 Méthode: " + method + "\n" +
+"📄 Référence: " + ref + "\n\n" +
+"💸 💳 PAYMENT NUMBERS:\n" +
+"1️⃣ 0329768376 - JEAN FREDERIC\n" +
+"2️⃣ 0378138688 - Marthe\n" +
+"3️⃣ 0342981576 - Marthe";
 
 window.open("https://wa.me/" + number + "?text=" + encodeURIComponent(message), "_blank");
 
