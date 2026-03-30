7ecb25baf76b2ebeaab48ebdd19767aa22d2c2ff
zunorabj-alt <zunorabj@gmail.com>
2026-03-30 18:09:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let selectedPack = "";
+let selectedNumbers = [];
 
 function showTab(tab){
 document.getElementById("ff").classList.add("hidden");
@@ -18,18 +19,47 @@ document.getElementById("formBox").classList.remove("hidden");
 
 document.getElementById("selectedPack").innerText = "🎮 Pack: " + pack;
 
-/* 💳 PAYMENT INFO + EMOJIS */
-document.getElementById("msg").innerText =
-"📲 💳 PAIEMENT INFOS:\n\n" +
-"1️⃣ 0329768376\n   👤 JEAN FREDERIC RABENJANIRINA\n\n" +
-"2️⃣ 0378138688\n   👤 Marthe\n\n" +
-"3️⃣ 0342981576\n   👤 Marthe\n\n" +
-"⚠️ Alefaso amin’ny iray amin’ireo numéro ireo ny vola";
+// reset message
+document.getElementById("msg").innerText = "💳 Safidio ny méthode paiement";
 }
 
 function closeOrder(){
 document.getElementById("overlay").classList.add("hidden");
 document.getElementById("formBox").classList.add("hidden");
+}
+
+/* 🔥 UPDATE PAYMENT SELON CHOIX */
+function updatePaymentInfo(){
+
+let method = document.getElementById("method").value;
+
+if(method === "orange"){
+selectedNumbers = [
+"0329768376 - JEAN FREDERIC",
+"0378138688 - Marthe"
+];
+
+document.getElementById("msg").innerText =
+"📲 ORANGE MONEY:\n\n" +
+"1️⃣ 0329768376 - JEAN FREDERIC\n" +
+"2️⃣ 0378138688 - Marthe";
+}
+
+else if(method === "mvola"){
+selectedNumbers = [
+"0342981576 - Marthe"
+];
+
+document.getElementById("msg").innerText =
+"📲 MVOLA:\n\n" +
+"1️⃣ 0342981576 - Marthe";
+}
+
+else{
+selectedNumbers = [];
+document.getElementById("msg").innerText = "";
+}
+
 }
 
 function sendOrder(){
@@ -38,12 +68,15 @@ let uid = document.getElementById("uid").value;
 let method = document.getElementById("method").value;
 let ref = document.getElementById("ref").value;
 
-if(uid === "" || ref === ""){
-document.getElementById("msg").innerText = "❌ Fenoy daholo azafady";
+if(uid === "" || ref === "" || selectedNumbers.length === 0){
+document.getElementById("msg").innerText = "❌ Fenoy tsara + safidio méthode";
 return;
 }
 
 let number = "261344723083";
+
+/* 🔥 ataovy string ny numéro voafidy */
+let numbersText = selectedNumbers.join("\n");
 
 let message =
 "🛒 💯 UP ZONE STORE 💳\n\n" +
@@ -52,9 +85,7 @@ let message =
 "💳 Méthode: " + method + "\n" +
 "📄 Référence: " + ref + "\n\n" +
 "💸 💳 PAYMENT NUMBERS:\n" +
-"1️⃣ 0329768376 - JEAN FREDERIC\n" +
-"2️⃣ 0378138688 - Marthe\n" +
-"3️⃣ 0342981576 - Marthe";
+numbersText;
 
 window.open("https://wa.me/" + number + "?text=" + encodeURIComponent(message), "_blank");
 
